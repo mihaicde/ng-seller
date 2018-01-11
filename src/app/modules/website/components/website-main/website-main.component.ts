7b83caaf21084fa9b5d02aa6db55fa60c14294bf
GEORGESCU <flo_georgescu@hotmail.com>
2018-01-11 18:55:35 +0200

    Created animation

--- src/app/modules/website/components/website-main/website-main.component.ts
+++ src/app/modules/website/components/website-main/website-main.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { routerTransition } from '../../../../utils/router.animations';
 import { AboutUsPageComponent } from '../about-us-page/about-us-page.component';
 
 import { ProgressWidthService } from '../../services/progress-width.service';
 
 @Component({
   selector: 'app-website-main',
-  templateUrl: './website-main.component.html'
+  animations: [ routerTransition ],
+  templateUrl: './website-main.component.html',
+  styleUrls: ['./website-main.component.scss']
 })
 export class WebsiteMainComponent implements OnInit {
  
   constructor(
     private progressService: ProgressWidthService
   ) { }
+
+  getState(outlet) {
+    return outlet.activatedRouteData.state;
+  }
 
   ngOnInit() {
    
